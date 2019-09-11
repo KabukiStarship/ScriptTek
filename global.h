d4fb491c0478f90be5269b7df2430612d818c0db
Cale McCollough <you@example.com>
2019-09-11 07:44:14 -0700

    Changes.Add existing files and Markdown templates. #1

---
+++ global.h
@@ -0,0 +1,17 @@
+/* Kabuki Tek @version 0.x
+@link    https://github.com/kabuki-starship/kabuki.tek.git
+@file    /kabuki/sdk/tek/c_portalwifi.h
+@author  Cale McCollough <<https://calemccollough.github.io>>
+@license Copyright (C) 2014-9 Cale McCollough <<calemccollough.github.io>>;
+All right reserved (R). This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with
+this file, You can obtain one at <https://mozilla.org/MPL/2.0/>. */
+
+#pragma once
+#include <pch.h>
+#ifndef INCLUDED_KABUKI_FEATURE_TEK_GLOBAL
+#define INCLUDED_KABUKI_FEATURE_TEK_GLOBAL 1
+
+#include "c_unicontroller.h"
+
+#endif
---
+++ tek_dmxbuffer.cc
@@ -0,0 +1,19 @@
+/* Kabuki Tek @version 0.x
+@link    https://github.com/kabuki-starship/kabuki.tek.git
+@file    /tek/tek_dmxbuffer.cc
+@author  Cale McCollough <https://calemccollough.github.io>
+@license Copyright (C) 2014-9 Cale McCollough <<calemccollough.github.io>>;
+All right reserved (R). This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with
+this file, You can obtain one at <https://mozilla.org/MPL/2.0/>. */
+
+#include "c_dmxbuffer.h"
+
+namespace _ {
+
+
+DMXBuffer::DMXBuffer () {
+
+}
+
+}       //< namespace _
